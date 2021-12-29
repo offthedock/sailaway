82e8ccc849a1905c78766ac693578265316b8120
Amine CHERRABI <amine.cherrabi@usmba-fpt.ac.ma>
2021-12-29 13:09:04 +0100

    Summary commit ...

---
+++ virt.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ARGs="-enable-kvm -rtc base=localtime -vga std"
+ARGs="${ARGs} -drive file=/dev/$1,readonly,cache=none,format=raw,if=virtio"
+ARGs="${ARGs} -m $2G"
+
+qemu-system-x86_64 $ARGs
